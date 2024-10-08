9947b2484845a69e00f30dab9b0eca76f017530e
Zord77 <210711036@students.uajy.ac.id>
2024-10-08 17:19:13 +0700

    tes

---
+++ TAM.VMS.Web/Areas/VendorRegistration/Controllers/VendorRegistrationController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TAM.VMS.Domain;
+using TAM.VMS.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Kendo.Mvc.UI;
+using TAM.VMS.Infrastructure.Session;
+
+namespace TAM.VMS.Web.Areas.VendorRegistration.Controllers
+{
+    [Area("VendorRegistration")]
+    [Authorize]
+    [AppAuthorize("App.VendorRegistration")]
+    public class VendorRegistrationController : WebController
+    {
+        public IActionResult Index()
+        {
+            var roles = Service<RoleService>().GetRoles();
+
+            ViewBag.Roles = roles;
+
+            return View();
+        }
+
+        public IActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = Service<VendorRegistrationService>().GetDataSourceResult(request);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult SaveUser(User user, IEnumerable<string> roles)
+        {
+            var result = Service<VendorRegistrationService>().Save(user, roles);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult ResetPasswordUser(ResetPassword user)
+        {
+            user.ResetNewPW = MD5Helper.Encode(user.ResetNewPW);
+            user.ResetConfirmNewPW = MD5Helper.Encode(user.ResetConfirmNewPW);
+            string result = string.Empty;
+            if (user.ResetNewPW != user.ResetConfirmNewPW)
+            {
+                result = "New password and Confirm password doesn't matched";
+            }
+            else
+            {
+                result = Service<VendorRegistrationService>().ResetPassword(user);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult ChangePasswordUser(ChangePassword user)
+        {
+            var dataUser = Service<VendorRegistrationService>().GetUserByUsername(SessionManager.Current);
+            user.Id = dataUser.ID;
+            string exsPW = dataUser.Password;
+            user.CurrentPW = MD5Helper.Encode(user.CurrentPW);
+            user.NewPW = MD5Helper.Encode(user.NewPW);
+            user.ConfirmNewPW = MD5Helper.Encode(user.ConfirmNewPW);
+            string result = string.Empty;
+            if (user.CurrentPW != exsPW)
+            {
+                result = "Current password is wrong";
+            }
+            else if (user.NewPW != user.ConfirmNewPW)
+            {
+                result = "New password and Confirm password doesn't matched";
+            }
+            else
+            {
+                result = Service<VendorRegistrationService>().ChangePassword(user);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUser(Guid id)
+        {
+            Service<VendorRegistrationService>().Delete(id);
+            return Ok();
+        }
+    }
+}
